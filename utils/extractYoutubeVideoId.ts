18533b39e846fc1003f6ef68ee8c84120b0cee99
sho kawamoto <k.sho626626@gmail.com>
2024-10-21 05:02:37 +0900

    done

--- utils/extractYoutubeVideoId.ts
+++ utils/extractYoutubeVideoId.ts
@@ -1,16 +1,12 @@
-
 export function extractYouTubeVideoId(url: string): string | null {
-    const matched =
-      /^https?:\/\/(www\.)?youtube\.com\/watch\?(.*&)?v=(?<videoId>[^&]+)/.exec(
-        url
-      ) ??
-      /^https?:\/\/youtu\.be\/(?<videoId>[^?]+)/.exec(url) ??
-      /^https?:\/\/(www\.)?youtube\.com\/embed\/(?<videoId>[^?]+)/.exec(url);
-  
-    if (matched?.groups?.videoId) {
-      return matched.groups.videoId;
-    } else {
-      return null;
-    }
+  const matched =
+    /^https?:\/\/(www\.)?youtube\.com\/watch\?(.*&)?v=([^&]+)/.exec(url) ??
+    /^https?:\/\/youtu\.be\/([^?]+)/.exec(url) ??
+    /^https?:\/\/(www\.)?youtube\.com\/embed\/([^?]+)/.exec(url);
+
+  if (matched && matched[3]) {
+    return matched[3];
+  } else {
+    return null;
   }
-  
+}
